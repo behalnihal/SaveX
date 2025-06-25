23524ccfa3e2a838c34a6fc3e1ded861260635d9
behalnihal <nihalgupta140@gmail.com>
2025-06-26 01:11:24 +0530

    feat: add logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="backdrop-blur-md bg-white/70 shadow-md border border-gray-200 rounded-b-xl px-8 py-4 flex justify-center items-center w-full">
-      <Link href="/">
-        <span className="text-2xl font-extrabold text-blue-600 tracking-tight drop-shadow-sm">
+    <nav className="backdrop-blur-md bg-white/70 shadow-md border border-gray-200 rounded-b-xl py-2 flex justify-center items-center w-full">
+      <Link href="/" className="flex items-center">
+        <Image src="/savex.png" alt="SaveX Logo" height={60} width={60}></Image>
+        <span className="text-2xl font-extrabold text-blue-400 tracking-tight drop-shadow-sm">
           SaveX
         </span>
       </Link>
